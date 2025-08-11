95ff146af11d54a609dff308a48bcc9ef49903de
Amarre <codeTechvian>
2025-08-12 00:33:50 +0800

    new

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'Amarre Jake.dart';
+import 'Abegael.dart';
+import 'Jhasmine Fate.dart';
+import 'Jeren.dart';
+import 'Sherry Ann.dart';
+
+void main() {
+  var amarre = Amarre_Jake('Amarre Jake', 19);
+  amarre.discribe();
+
+  var abegael = Abegael('Abegael', 21);
+  abegael.discribe();
+
+  var jhasmine = Jhasmine_Fate('Jhasmine Fate', 19);
+  jhasmine.discribe();
+
+  var jeren = Jeren('Jeren', 21);
+  jeren.discribe();
+
+  var sherryAnn = Sherry_Ann('Sherry Ann', 22);
+  sherryAnn.discribe();
+}
